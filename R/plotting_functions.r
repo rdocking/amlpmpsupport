e732c74d4042e3f9840e9fd30fcfea3b41ffa57c
Rod Docking <rdocking@bcgsc.ca>
2017-06-29 13:30:30 -0700

    Add function for plotting things with a shared legend, update documentation

--- R/plotting_functions.r
+++ R/plotting_functions.r
@@ -131,3 +131,39 @@ subset_heatmap <- function(gene_set, annotation_col, counts.mat){
   return(p)
 }
 
+#' Generate a grid of plots with a shared legend
+#'
+#' Adapted from:
+#'   https://github.com/tidyverse/ggplot2/wiki/Share-a-legend-between-two-ggplot2-graphs
+#'   http://rpubs.com/sjackman/grid_arrange_shared_legend
+#'   http://stackoverflow.com/questions/13649473/add-a-common-legend-for-combined-ggplots
+#'
+#' @param ... A set of plots to arrange with a shared legend
+#'
+#' @return A ggplot plot
+#' @export
+#'
+grid_arrange_shared_legend <- function(..., ncol = length(list(...)), nrow = 1, position = c("bottom", "right")) {
+  plots <- list(...)
+  position <- match.arg(position)
+  g <- ggplotGrob(plots[[1]] + theme(legend.position = position))$grobs
+  legend <- g[[which(sapply(g, function(x) x$name) == "guide-box")]]
+  lheight <- sum(legend$height)
+  lwidth <- sum(legend$width)
+  gl <- lapply(plots, function(x) x + theme(legend.position="none"))
+  gl <- c(gl, ncol = ncol, nrow = nrow)
+  combined <- switch(position,
+                     "bottom" = arrangeGrob(do.call(arrangeGrob, gl),
+                                            legend,
+                                            ncol = 1,
+                                            heights = unit.c(unit(1, "npc") - lheight, lheight)),
+                     "right" = arrangeGrob(do.call(arrangeGrob, gl),
+                                           legend,
+                                           ncol = 2,
+                                           widths = unit.c(unit(1, "npc") - lwidth, lwidth)))
+  grid.newpage()
+  grid.draw(combined)
+  # return gtable invisibly
+  invisible(combined)
+}
+
