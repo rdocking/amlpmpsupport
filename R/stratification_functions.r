eca1709f675e4dedc10e80293f07785b341a11f0
Rod Docking <rdocking@bcgsc.ca>
2018-09-25 11:51:07 -0700

    Add first pass at ELN2015-Cyto

--- R/stratification_functions.r
+++ R/stratification_functions.r
@@ -45,15 +45,11 @@ apply_eln2017_cyto <-
   if (cyto_status %in% c('t_15_17', 'cbf_fusion')) {
     status <- 'favourable'
     rationale <- 'Favourable cytogenetics'
-    # Poor-risk cytogenetics
+  # Poor-risk cytogenetics
   } else if (cyto_status == 'adverse_risk') {
     status <- 'adverse'
     rationale <- 'Adverse-risk cytogenetics'
-    #
-    #} else if (cyto_status == 'intermediate_risk') {
-    #    status <- 'intermediate'
-    #    rationale <- 'Intermediate-risk cytogenetics'
-    # Intermediate-risk cyto and normal karyotype - check mutations
+  # Intermediate-risk cyto and normal karyotype - check mutations
   } else if (cyto_status %in% c('normal_karyotype', 'intermediate_risk', 'missing')) {
 
     status <- 'intermediate'
@@ -89,3 +85,75 @@ apply_eln2017_cyto <-
   # Return the status and rationale
   return(data_frame(status = status, rationale = rationale))
 }
+
+
+#' Apply ELN2015 stratification based on cytogenetics
+#'
+#' @param cyto_status Cytogenetic status
+#' @param npm1 NPM1 status
+#' @param flt3_itd FLT3-ITD status
+#' @param cebpa CEBPA status
+#' @param tp53 TP53 status
+#' @param runx1 RUNX1 status
+#' @param asxl1 ASXL1 status
+#'
+#' @return A stratification status and rationale
+#' @export
+#'
+#' @examples
+apply_eln2015_cyto <- function(cyto_status, npm1, flt3_itd, cebpa){
+
+    # Treat NA values as 'missing'
+    if (is.na(cyto_status)) cyto_status = 'missing'
+    if (is.na(npm1)) npm1 = 'missing'
+    if (is.na(cebpa)) cebpa = 'missing'
+    if (is.na(tp53)) tp53 = 'missing'
+    if (is.na(asxl1)) asxl1 = 'missing'
+    if (is.na(runx1)) runx1 = 'missing'
+    if (is.na(flt3_itd)) flt3_itd = 'missing'
+
+    # Convert all values except 'positive' for FLT3-ITD to 'negative'
+    if (!flt3_itd == 'positive') flt3_itd = 'negative'
+
+    # Start as intermediate, no rules applied
+    status <- 'intermediate-II'
+    rationale <- 'No rules applied'
+
+    # Check cytogenetics, then mutations
+    if (cyto_status %in% c('t_15_17', 'cbf_fusion')) {
+      status <- 'favourable'
+      rationale <- 'Favourable cytogenetics'
+    # Poor-risk cytogenetics
+    } else if (cyto_status == 'adverse_risk') {
+      status <- 'adverse'
+      rationale <- 'Adverse-risk cytogenetics'
+    # Intermediate-risk cyto and normal karyotype - check mutations
+    } else if (cyto_status %in% c('normal_karyotype', 'intermediate_risk', 'missing')) {
+
+      status <- 'intermediate-II'
+      rationale <- 'Intermediate-risk cytogenetics'
+
+      # True intermediates
+      if (cyto_status %in% 'intermediate_risk') {
+        status <- 'intermediate-II'
+        rationale <- 'Intermediate-risk cytogenetics'
+      # CEBPA biallelic
+      } else if (cebpa == 'CEBPAbi') {
+        status <- 'favourable'
+        rationale <- 'Intermediate SVs, biallelic CEBPA'
+      # Then FLT3 and NPM1
+      } else if (npm1 == 'NPM1fs' & flt3_itd == 'negative') {
+        status <- 'favourable'
+        rationale <- 'Intermediate SVs, NPM1+FLT3-'
+      # Then FLT3 positive
+      } else if (npm1 == 'NPM1fs' & flt3_status == 'positive') {
+        status <- 'intermediate'
+        rationale <- 'Intermediate SVs, NPM1+FLT3+'
+      } else if (flt3_status == 'positive') {
+        status <- 'adverse'
+        rationale <- 'Intermediate SVs, NPM1-FLT3+'
+      }
+    }
+    # Return the status and rationale
+    return(data_frame(status = status, rationale = rationale))
+  }
