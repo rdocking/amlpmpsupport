84fff461ea6066dece617779813bb9beda91d748
Rod Docking <rdocking@bcgsc.ca>
2018-12-21 11:49:01 -0800

    Add function for converting numbers to text

--- R/utility_functions.r
+++ R/utility_functions.r
@@ -80,3 +80,42 @@ matrix_glimpse <- function(mat) {
 #'
 #' @examples select_by_position(lst, 1)
 select_by_position <- function(x, position) x[position]
+
+
+#' Convert a small number to a text representation
+#'
+#' @param x An integer to convert to text
+#' @param capitalize Boolean whether to capitalize text
+#'
+#' @return
+#' @export
+#'
+#' @examples num_to_text(5, capitalize = TRUE)
+num_to_text <- function(x, capitalize = FALSE){
+
+  # Input checks
+  assertthat::is.number(x)
+  assertthat::assert_that(x <= 10)
+
+  num_table <- tibble::tribble(
+    ~x, ~small,
+    1, 'one',
+    2, 'two',
+    3, 'three',
+    4, 'four',
+    5, 'five',
+    6, 'six',
+    7, 'seven',
+    8, 'eight',
+    9, 'nine',
+    10, 'ten') %>%
+    dplyr::mutate(big = stringr::str_to_title(small))
+
+  if (capitalize) {
+    ret <- num_table[x,]$big
+  } else {
+    ret <- num_table[x,]$small
+  }
+  return(ret)
+}
+
