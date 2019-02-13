7cb08f21727733297e179b9a7cbbf20a287fe472
Rod Docking <rdocking@bcgsc.ca>
2019-02-13 10:03:52 -0800

    Add in modified survival plot function with updated sizes

--- R/survival_functions.r
+++ R/survival_functions.r
@@ -28,3 +28,47 @@ plot_survival <- function(exp.design, label_col){
   p <- GGally::ggsurv(surv, CI = TRUE) + ggplot2::ggtitle('Kaplan-Meier Estimator')
   p
 }
+
+#' Modified survival plot
+#'
+#' @param surv.fit A survival object from survival::Surv
+#' @param data The data frame used to generate the survival object
+#' @param palette A named list containing plotting colours
+#' @param title Plot title
+#' @param ... Additional parameters to pass to ggsurvplot
+#'
+#' @return
+#' @export
+surv_panel_plot <- function(surv.fit, data, palette, title, pval.coord = c(600, 0.9), xlim = c(0, 800), ...){
+  p <- survminer::ggsurvplot(surv.fit,
+                  data = data,
+                  # conf.int = TRUE,        # Add confidence interval
+                  pval = TRUE,              # Add p-value
+                  pval.coord = pval.coord,
+                  pval.method = FALSE,
+                  risk.table = TRUE,        # Add risk table
+                  risk.table.col = "strata",  # Risk table color by groups)
+                  risk.table.y.text = FALSE,
+                  risk.table.height = 0.2,
+                  risk.table.title = "",
+                  ggtheme = theme_bw(),
+                  ncensor.plot = FALSE,
+                  legend = "none",
+                  palette = palette,
+                  legend.title = "",
+                  xlab = "Time (Weeks)",
+                  xlim = xlim,
+                  break.x.by = 100,
+                  font.legend = c(16),
+                  font.main = c(20),
+                  font.x = c(16),
+                  font.y = c(16),
+                  font.tickslab = c(16),
+                  title = title,
+                  surv.median.line = "none",
+                  ...)
+
+  # Hack into the object to tweak further
+  p[["table"]][["labels"]][["x"]] <- ""
+  p
+}
