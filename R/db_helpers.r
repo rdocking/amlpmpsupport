bff48e9558f78abcaa778290083880ea7789d6dc
Rod Docking <roddocking@gmail.com>
2020-06-17 17:14:41 -0700

    Continue cleanup of DB functions

--- R/db_helpers.r
+++ R/db_helpers.r
@@ -34,35 +34,36 @@ retrieve_db_libraries <- function(db_conn) {
   platform.tbl <- dplyr::tbl(db_conn, "platform")
   # Start from patient, and query downwards
   db_libraries.query <- patient.tbl %>%
-    dplyr::select(id, .data$external_id, is_patient) %>%
-    dplyr::rename(patient_id = id) %>%
+    dplyr::select(.data$id, .data$external_id, .data$is_patient) %>%
+    dplyr::rename(patient_id = .data$id) %>%
     dplyr::rename(patient_external_id = .data$external_id) %>%
     # Join to specimen
     dplyr::left_join(specimen.tbl, by = "patient_id") %>%
-    dplyr::rename(specimen_id = id) %>%
+    dplyr::rename(specimen_id = .data$id) %>%
     dplyr::rename(specimen_external_id = .data$external_id) %>%
-    dplyr::select(-patient_id, -meta) %>%
+    dplyr::select(-.data$patient_id, -.data$meta) %>%
     # Join to specimen_subset
     dplyr::left_join(specimen_subset.tbl, by = "specimen_id") %>%
-    dplyr::rename(specimen_subset_id = id) %>%
+    dplyr::rename(specimen_subset_id = .data$id) %>%
     dplyr::rename(specimen_subset_external_id = .data$external_id) %>%
     # Join to library
-    left_join(library.tbl, by = "specimen_subset_id") %>%
-    dplyr::rename(library_id = id) %>%
-    dplyr::rename(library_name = name) %>%
+    dplyr::left_join(library.tbl, by = "specimen_subset_id") %>%
+    dplyr::rename(library_id = .data$id) %>%
+    dplyr::rename(library_name = .data$name) %>%
     # Join to platform and platform_version
     dplyr::left_join(platform_version.tbl,
                      by = c("platform_version_id" = "id")) %>%
-    dplyr::rename(platform_version_name = name) %>%
+    dplyr::rename(platform_version_name = .data$name) %>%
     dplyr::left_join(platform.tbl,
                      by = c("platform_id" = "id")) %>%
-    dplyr::rename(platform_name = name) %>%
-    dplyr::select(-fields, -platform_id, -platform_version_id, -specimen_subset_id,
-                  -specimen_id, -library_id, -sequencing_effort_id,
-                  -library_qc_info, -type) %>%
+    dplyr::rename(platform_name = .data$name) %>%
+    dplyr::select(-.data$fields, -.data$platform_id, -.data$platform_version_id,
+                  -.data$specimen_subset_id, -.data$specimen_id,
+                  -.data$library_id, -.data$sequencing_effort_id,
+                  -.data$library_qc_info, -.data$type) %>%
     # Filter out rows with no library name
-    dplyr::filter(!is.na(library_name))
-  db_libraries.df <- collect(db_libraries.query)
+    dplyr::filter(!is.na(.data$library_name))
+  db_libraries.df <- dplyr::collect(db_libraries.query)
   return(db_libraries.df)
 }
 
@@ -86,58 +87,59 @@ retrieve_db_paths <- function(db_conn) {
   method.tbl <- dplyr::tbl(db_conn, "method")
   pipeline_version.tbl <- dplyr::tbl(db_conn, "pipeline_version")
   # Start from patient, and query downwards
-  patient.tbl %>%
-    dplyr::select(id, .data$external_id, is_patient) %>%
-    dplyr::rename(patient_id = id) %>%
+  db_paths.query <-
+    patient.tbl %>%
+    dplyr::select(.data$id, .data$external_id, .data$is_patient) %>%
+    dplyr::rename(patient_id = .data$id) %>%
     dplyr::rename(patient_external_id = .data$external_id) %>%
     # Join to specimen
     dplyr::left_join(specimen.tbl, by = "patient_id") %>%
-    dplyr::rename(specimen_id = id) %>%
+    dplyr::rename(specimen_id = .data$id) %>%
     dplyr::rename(specimen_external_id = .data$external_id) %>%
-    dplyr::select(-patient_id, -meta) %>%
+    dplyr::select(-.data$patient_id, -.data$meta) %>%
     # Join to specimen_subset
     dplyr::left_join(specimen_subset.tbl, by = "specimen_id") %>%
-    dplyr::rename(specimen_subset_id = id) %>%
+    dplyr::rename(specimen_subset_id = .data$id) %>%
     dplyr::rename(specimen_subset_external_id = .data$external_id) %>%
     # Join to library
-    left_join(library.tbl, by = "specimen_subset_id") %>%
-    dplyr::rename(library_id = id) %>%
-    dplyr::rename(library_name = name) %>%
+    dplyr::left_join(library.tbl, by = "specimen_subset_id") %>%
+    dplyr::rename(library_id = .data$id) %>%
+    dplyr::rename(library_name = .data$name) %>%
     # Join to platform and platform_version
     dplyr::left_join(platform_version.tbl,
                      by = c("platform_version_id" = "id")) %>%
-    dplyr::rename(platform_version_name = name) %>%
+    dplyr::rename(platform_version_name = .data$name) %>%
     dplyr::left_join(platform.tbl,
                      by = c("platform_id" = "id")) %>%
-    dplyr::rename(platform_name = name) %>%
+    dplyr::rename(platform_name = .data$name) %>%
     # Join to analysis
     dplyr::left_join(analysis.tbl,
                      by = c("library_id")) %>%
-    dplyr::rename(analysis_id = id) %>%
+    dplyr::rename(analysis_id = .data$id) %>%
     # Join to path
     dplyr::left_join(path.tbl,
                      by = c("analysis_id")) %>%
-    dplyr::rename(path_id = id) %>%
+    dplyr::rename(path_id = .data$id) %>%
     # Join to filetype, method, and pipeline version
     dplyr::left_join(filetype.tbl,
                      by = c("filetype_id" = "id")) %>%
-    dplyr::rename(filetype_name = name) %>%
+    dplyr::rename(filetype_name = .data$name) %>%
     dplyr::left_join(method.tbl,
                      by = c("method_id" = "id")) %>%
-    dplyr::rename(method_name = name) %>%
+    dplyr::rename(method_name = .data$name) %>%
     dplyr::left_join(pipeline_version.tbl,
                      by = c("pipeline_version_id" = "id")) %>%
-    dplyr::rename(pipeline_version_name = name) %>%
+    dplyr::rename(pipeline_version_name = .data$name) %>%
     # Remove rows that have no path information
-    dplyr::filter(!is.na(path)) %>%
+    dplyr::filter(!is.na(.data$path)) %>%
     # Select out UUID fields and unused fields
-    dplyr::select(-fields, -platform_id, -platform_version_id, -specimen_subset_id,
-                  -specimen_id, -library_id, -sequencing_effort_id,
-                  -library_qc_info, -type, -description,
-                  -path_id, -filetype_id, -method_id, -pipeline_version_id,
-                  -analysis_id) ->
-    db_paths.query
-  db_paths.df <- collect(db_paths.query)
+    dplyr::select(-.data$fields, -.data$platform_id, -.data$platform_version_id,
+                  -.data$specimen_subset_id, -.data$specimen_id,
+                  -.data$library_id, -.data$sequencing_effort_id,
+                  -.data$library_qc_info, -.data$type, -.data$description,
+                  -.data$path_id, -.data$filetype_id, -.data$method_id,
+                  -.data$pipeline_version_id, -.data$analysis_id)
+  db_paths.df <- dplyr::collect(db_paths.query)
   return(db_paths.df)
 }
 
@@ -182,17 +184,17 @@ retrieve_db_curated_results <- function(db_conn) {
     dplyr::inner_join(patient.tbl, by = c("patient_id" = "id")) %>%
     dplyr::rename(patient_external_id = .data$external_id) %>%
     # Select matching fields
-    dplyr::select(patient_external_id, specimen_external_id,
-                  subtype, flt3_status,
-                  npm1_status, kit_status, cebpa_status,
-                  cytogenetic_risk, karyotype, fusions,
-                  tier_one_mutation_status, kmt2a_status,
-                  dnmt3a_status) %>%
+    dplyr::select(.data$patient_external_id, .data$specimen_external_id,
+                  .data$subtype, .data$flt3_status,
+                  .data$npm1_status, .data$kit_status, .data$cebpa_status,
+                  .data$cytogenetic_risk, .data$karyotype, .data$fusions,
+                  .data$tier_one_mutation_status, .data$kmt2a_status,
+                  .data$dnmt3a_status) %>%
     # Sort by specimen ID
-    arrange(specimen_external_id) ->
+    dplyr::arrange(.data$specimen_external_id) ->
     curated_results.query
     # Collect results into a data frame
-    curated_results.df <- collect(curated_results.query)
+    curated_results.df <- dplyr::collect(curated_results.query)
     # Add a simplified disease type
     curated_results.df$disease <- purrr::map_chr(curated_results.df$subtype,
                                                  subtype_to_disease)
@@ -211,22 +213,23 @@ retrieve_db_specimens <- function(db_conn) {
   specimen.tbl <- dplyr::tbl(db_conn, "specimen")
   specimen_subset.tbl <- dplyr::tbl(db_conn, "specimen_subset")
   # Start from patient, and query downwards
-  patient.tbl %>%
-    dplyr::select(id, .data$external_id, is_patient) %>%
-    dplyr::rename(patient_id = id) %>%
+  db_specimens.query <-
+    patient.tbl %>%
+    dplyr::select(.data$id, .data$external_id, .data$is_patient) %>%
+    dplyr::rename(patient_id = .data$id) %>%
     dplyr::rename(patient_external_id = .data$external_id) %>%
     # Join to specimen
     dplyr::left_join(specimen.tbl, by = "patient_id") %>%
-    dplyr::rename(specimen_id = id) %>%
+    dplyr::rename(specimen_id = .data$id) %>%
     dplyr::rename(specimen_external_id = .data$external_id) %>%
-    dplyr::select(-patient_id) %>%
+    dplyr::select(-.data$patient_id) %>%
     # Join to specimen_subset
     dplyr::left_join(specimen_subset.tbl, by = "specimen_id") %>%
-    dplyr::rename(specimen_subset_id = id) %>%
+    dplyr::rename(specimen_subset_id = .data$id) %>%
     dplyr::rename(specimen_subset_external_id = .data$external_id) %>%
-    dplyr::select(-specimen_subset_id, -specimen_id) ->
-    db_specimens.query
-  db_specimens.df <- collect(db_specimens.query)
+    dplyr::select(-.data$specimen_subset_id, -.data$specimen_id)
+
+  db_specimens.df <- dplyr::collect(db_specimens.query)
   return(db_specimens.df)
 }
 
@@ -252,10 +255,10 @@ retrieve_db_comments <- function(db_conn) {
     dplyr::inner_join(patient.tbl, by = c("patient_id" = "id")) %>%
     dplyr::rename(patient_external_id = .data$external_id) %>%
     # Select matching fields
-    dplyr::select(patient_external_id, specimen_external_id,
-                  comment, added_by, added_date) ->
+    dplyr::select(.data$patient_external_id, .data$specimen_external_id,
+                  .data$comment, .data$added_by, .data$added_date) ->
     curated_comments.query
   # Collect results into a data frame
-  curated_comments.df <- collect(curated_comments.query)
+  curated_comments.df <- dplyr::collect(curated_comments.query)
   return(curated_comments.df)
 }
